d0cb7d30784fd03ef021c0f932a810a3fec71ee0
Declan Talbert <declan_at@hotmail.com>
2020-10-17 18:09:26 +0100

    41. Connect MongoDB To Our App - Completed

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.json({msg: 'Welcome to the ContactKeep API...'}));
 
-//Define Routes
+// Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
