674828d21a37a59edc079f85adb1c5d3a25de166
Declan Talbert <declan_at@hotmail.com>
2020-11-04 23:04:10 +0000

    auth.js line 35 typo fixed

--- routes/auth.js
+++ routes/auth.js
@@ -32,7 +32,7 @@ router.post('/',  [
             if (!user) {
                 return res.status(400).json({ msg: 'Invalid Credentials' });
             }
-            const isMatch = await bcrypt.compare(password, user.pasword);
+            const isMatch = await bcrypt.compare(password, user.password);
             if(!isMatch) {
                 return res.status(400).json({ msg: 'Invalid Credentials' });
             }
