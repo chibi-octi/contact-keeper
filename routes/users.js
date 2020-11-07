1fab0e901105389669bb28ea22529b55b31734dc
Declan Talbert <declan_at@hotmail.com>
2020-11-07 22:06:36 +0000

    package.json updates

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const { check, validationResult } = require('express-validator');
+const {check, validationResult} = require('express-validator');
 
 const User = require('../models/User');
 
@@ -20,21 +20,21 @@ router.post(
         check(
             'password', 
             'Please enter a password with 6 or more characters')
-            .isLength({ min: 6 })
+            .isLength({min: 6})
     ], 
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            return res.status(400).json({errors: errors.array()});
         }
 
-        const { name, email, password } = req.body;
+        const {name, email, password} = req.body;
 
         try {
-            let user = await User.findOne({ email });
+            let user = await User.findOne({email});
 
             if (user) {
-                return res.status(400).json({ msg: 'User already exisits' });
+                return res.status(400).json({msg: 'User already exisits'});
             }
 
             user = new User({
@@ -63,7 +63,7 @@ router.post(
                 }, 
                 (err, token) => {
                     if (err) throw err;
-                    res.json({ token });
+                    res.json({token});
                 }
             );
         } catch (err) {
@@ -71,6 +71,7 @@ router.post(
             res.status(500).send('Server Error');
         }
     }
+    
 );
 
 module.exports = router;
