bf80aab2382b9bcc16a699c7ae4ae918864e19f7
Declan Talbert <declan_at@hotmail.com>
2020-11-18 22:42:31 +0000

    51. Cleanup, Navbar & Router Setup - Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import Reac, { Fragment } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
+    <Router>
+    <Fragment>
       <Navbar />
-    </div>
+      <div className="container">
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+        </Switch>
+      </div>
+    </Fragment>
+    </Router>
   );
 }
 
